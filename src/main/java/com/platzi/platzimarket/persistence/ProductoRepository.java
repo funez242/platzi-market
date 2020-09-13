f7434ef9267f4bb97f21aad2405dafb45acfaf6e
JoseFunez <funez242@gmail.com>
2020-09-13 00:07:00 -0500

    Repository Completed

--- src/main/java/com/platzi/platzimarket/persistence/ProductoRepository.java
+++ src/main/java/com/platzi/platzimarket/persistence/ProductoRepository.java
@@ -2,13 +2,38 @@ package com.platzi.platzimarket.persistence;
 
 import com.platzi.platzimarket.persistence.crud.ProductoCrudRepository;
 import com.platzi.platzimarket.persistence.entity.Producto;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
+//como esta clase va a interactuar con la base de datos se le pone la anotacion de repository
+@Repositorygit status
 public class ProductoRepository {
     private ProductoCrudRepository productoCrudRepository;
 
     public List<Producto> getAll(){
         return (List<Producto>) productoCrudRepository.findAll();
     }
+
+    public List<Producto> getByCategoria(int idCategoria){
+        return productoCrudRepository.findByIdCategoria(idCategoria);
+    }
+
+    public Optional<List<Producto>> getProductosEscasos(int cantidad){
+        return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
+    }
+
+    public Optional<Producto> getProducto(int idProducto){
+        return productoCrudRepository.findById(idProducto);
+    }
+
+    public Producto save(Producto producto){
+        return productoCrudRepository.save(producto);
+    }
+
+    public void delete(Producto producto){
+         productoCrudRepository.deleteById(producto);
+    }
+
 }
--- src/main/java/com/platzi/platzimarket/persistence/crud/ProductoCrudRepository.java
+++ src/main/java/com/platzi/platzimarket/persistence/crud/ProductoCrudRepository.java
@@ -1,10 +1,27 @@
 package com.platzi.platzimarket.persistence.crud;
 
 import com.platzi.platzimarket.persistence.entity.Producto;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+import java.util.Optional;
 
 // Esta interfaz nos va a permitir hacer operaciones CRUD de nustros productos
 // Esta debe extender de CrudRepository y tener dentro de pico parentesis
 // el Nombre de la clase de producto y el tipo de dato de su clave primaria o Id
 public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
+    // El siguiente metodo hace uso de Query Methods que son una forma mas rapida y flexible de traernos datos de la base de datos
+    // Simplemente debe cumplic con cierta sintaxis como llevar el findBy seguido del nombre del atributo por el que queremos buscar
+    // escrito en camelCase y podriamos agregar mas atributos como orderBy... quedando -> findByIdCategoriaOrderByNombreAsc(int idCategoria)
+    List<Producto> findByIdCategoria(int idCategoria);
+
+    // Igual se podria Usar la anotacion @Query de la siguiente manera
+    @Query(value = "SELECT * FROM productos WHERE id_categoria = ?",nativeQuery = true);
+    List<Producto> getByCategoria(int idCategoria);//En este caso podemos nombrar el metodo como querramos
+
+    // Los query methods tambien soportan los operadores opcionales de la nuevas versiones de Java
+    // no olvifrmod que los parametros se deben llamar como estan en nuestra clase
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock, boolean estado);
+
 }
